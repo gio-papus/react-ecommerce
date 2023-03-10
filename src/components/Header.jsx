b93bc155bbd531b3565b8d94045fcdd1357949cb
Giorgi Meshveliani <xgorgaslax@gmail.com>
2023-03-10 17:23:58 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Logo from "../img/logoCuchillo.png"
+import Profile from "../img/davitiA.png"
+import "./header.css"
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
-}
-
+export const Header = () => {
+    return (
+      <>
+      <div className='header__div'>
+          <img className='logo__img--css' src={Logo} alt=""></img>
+          <input className='input__search' type="search"></input>
+          <img className='profile__img--css' src={Profile} alt=""></img>
+      </div>
+      </>
+    )
+  }
 export default Header
